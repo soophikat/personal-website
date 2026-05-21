b6c2200745fd86acd9b856bee48f637e566151ec
sapphickat <sphhhsh@gmail.com>
2026-05-21 12:00:07 -0300

    build steps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+      serverComponentsExternalPackages: ['better-sqlite3'],
+  },
 };
 
 export default nextConfig;
